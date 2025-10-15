5ff39f3b04f780e243ed07ed58bcc4ebd34de6fb
kamolan0608-pixel <kamolan0608@gmail.com>
2025-10-15 07:43:51 +0500

    Update main.py
    testing 5

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ async def send_weather():
 
 async def main():
     scheduler = AsyncIOScheduler(timezone=TIMEZONE)
-    times = [(7, 41), (7, 42), (7, 43)]
+    times = [(7, 50), (8, 10), (8, 20)]
     for hour, minute in times:
         trigger = CronTrigger(hour=hour, minute=minute, timezone=TIMEZONE)
         scheduler.add_job(send_weather, trigger=trigger, id=f"weather_{hour}_{minute}")
